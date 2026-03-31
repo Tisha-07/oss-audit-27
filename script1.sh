4eacde1f4b4d5efa67bc8a1b4e196a5fdaf306bb
Tisha-07 <tanisha.24bce10842@vitbhopal.ac.in>
2026-03-31 20:31:04 +0530

    System Identity Report

--- script1.sh
+++ script1.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+
+# Script 1: System Identity Report
+# Author: Tanisha Manshani
+# Course: Open Source Software
+
+# -------- Variables --------
 STUDENT_NAME="TANISHA"
 SOFTWARE="Git"
 
-KERNEL=$(uname -r)
-USER_NAME=$(whoami)
-UPTIME=$(uptime -p)
-DATE=$(date)
-DISTRO=$(lsb_release -d | cut -f2)
+# -------- System Information --------
+KERNEL=$(uname -r)                # Get kernel version
+USER_NAME=$(whoami)              # Get current user
+UPTIME=$(uptime -p)              # Get system uptime
+DATE=$(date)                     # Get current date and time
+DISTRO=$(lsb_release -d | cut -f2)  # Get Linux distribution name
+HOME_DIR=$HOME                   # Get home directory
 
+# -------- Display Output --------
 echo "=============================="
 echo " Open Source Audit - $STUDENT_NAME"
 echo "=============================="
 echo "Software : $SOFTWARE"
 echo "Kernel   : $KERNEL"
 echo "User     : $USER_NAME"
+echo "Home Dir : $HOME_DIR"
 echo "Uptime   : $UPTIME"
 echo "Date     : $DATE"
 echo "Distro   : $DISTRO"
-echo "License  : GPL License"
+echo "License  : GNU GPL v2"
