970c28168173d8e475cd6b65acaa160f0ef1f9ee
Tisha-07 <tanisha.24bce10842@vitbhopal.ac.in>
2026-03-31 20:32:30 +0530

    FOSS Package Inspector

--- script2.sh
+++ script2.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
+# Script 2: FOSS Package Inspector
+# Author: Tanisha Manshani
+# Course: Open Source Software
+
+# -------- Variable --------
 PACKAGE="git"
 
-if dpkg -l | grep -q $PACKAGE; then
+# -------- Check if package is installed --------
+if dpkg -s "$PACKAGE" &>/dev/null; then
     echo "$PACKAGE is installed."
-    git --version
+
+    # Display package details
+    dpkg -s "$PACKAGE" | grep -E "Version|Maintainer|Description"
+
 else
     echo "$PACKAGE is NOT installed."
 fi
 
+# -------- Case statement for philosophy --------
 case $PACKAGE in
-    git) echo "Git: Version control system for developers." ;;
-    firefox) echo "Firefox: Web browser." ;;
-    vlc) echo "VLC: Media player." ;;
-    mysql) echo "MySQL: Database system." ;;
+    git) echo "Git: A distributed version control system that empowers open collaboration." ;;
+    firefox) echo "Firefox: A web browser supporting an open and accessible internet." ;;
+    vlc) echo "VLC: A free multimedia player that supports all formats." ;;
+    mysql) echo "MySQL: An open-source database used in millions of applications." ;;
+    *) echo "Unknown package." ;;
 esac
